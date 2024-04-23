402c728358d759fdd59690c22ff87690e4e0565e
Marcos Rodríguez <e.marcosra@gmail.com>
2024-04-23 10:07:25 +0200

    Create type LevelRange, add aliases types to all characters

--- src/types.ts
+++ src/types.ts
@@ -1,40 +1,30 @@
-interface Character {
-  name: string;
-  surname: string;
-  age: number;
-  status: string;
-}
+type LevelRange = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
-interface King {
+type Character = {
   name: string;
   surname: string;
   age: number;
-  status: string;
+  status: "Dead" | "Alive";
+};
+
+type King = Character & {
   yearsReigning: number;
-}
+  sentence: "Vais a morir todos";
+};
 
-interface Fighter {
-  name: string;
-  surname: string;
-  age: number;
-  status: string;
+type Fighter = Character & {
   weapon: string;
-  dexterityLevel: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-}
+  dexterityLevel: LevelRange;
+  sentence: "Primero pego y luego pregunto";
+};
 
-interface Squire {
-  name: string;
-  surname: string;
-  age: number;
-  status: string;
+type Squire = Character & {
   characterWhomServes: Fighter;
-  asslickerLevel: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
-}
+  asslickerLevel: LevelRange;
+  sentence: "Soy un loser";
+};
 
-interface Adviser {
-  name: string;
-  surname: string;
-  age: number;
-  status: string;
+type Adviser = Character & {
   characterWhomAdvises: Squire | Fighter | King | Adviser;
-}
+  sentence: "No sé por qué, pero creo que voy a morir pronto";
+};
