35914f4438a53bb6d4624574ecbfc1e5b592330e
Marcos Rodríguez <e.marcosra@gmail.com>
2024-04-24 17:24:37 +0200

    Create createKingCharacter function and character with it

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 type Level = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+export type Sentence = string[];
 
 export interface Character {
   name: string;
@@ -11,22 +12,18 @@ export interface Character {
 
 export interface King extends Character {
   yearsReigning: number;
-  sentence: "Vais a morir todos";
 }
 
 export interface Fighter extends Character {
   dexterityLevel: Level;
   weapon: string;
-  sentence: "Primero pego y luego pregunto";
 }
 
 export interface Squire extends Character {
   characterWhomServes: Fighter;
   asslickerLevel: Level;
-  sentence: "Soy un loser";
 }
 
 export interface Adviser extends Character {
   characterWhomAdvises: Squire | Fighter | King | Adviser;
-  sentence: "No sé por qué, pero creo que voy a morir pronto";
 }
