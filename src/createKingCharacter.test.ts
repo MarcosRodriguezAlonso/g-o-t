c1c2e2821904d5216adb1e83dd814dc145b1ea0d
Marcos Rodríguez <e.marcosra@gmail.com>
2024-04-25 18:54:17 +0200

    Create sentence property for characters, variables for sentences and modify test

--- src/createKingCharacter.test.ts
+++ src/createKingCharacter.test.ts
@@ -1,9 +1,9 @@
-import { createKingCharacter } from "../charactersFactory";
-import { type King } from "../types";
+import { createKingCharacter } from "./charactersFactory";
+import { type King } from "./types";
 
 describe("Given the fucntion createKingCharacter", () => {
   describe("When it reciebes the name Marcos, the surname Rodríguez, the age 29, and the years reigning 2", () => {
-    test.only("Then it should return a 29 years old King called Marcos Rodríguez, who has been reignin for 2 years", () => {
+    test("Then it should return a 29 years old King called Marcos Rodríguez, who has been reignin for 2 years", () => {
       const name = "Marcos";
       const surname = "Rodríguez";
       const age = 29;
@@ -13,6 +13,7 @@ describe("Given the fucntion createKingCharacter", () => {
         surname,
         age,
         yearsReigning,
+        sentence: "Vais a morir todos",
       };
 
       const createdKing: King = createKingCharacter(
@@ -26,16 +27,16 @@ describe("Given the fucntion createKingCharacter", () => {
     });
   });
 
-  describe("When it recibes the name Marcos, the surname Rodríguez, the  age of 0, and the years reigning 0", () => {
+  describe("When it recibes the name Marcos, the surname Rodríguez, the  age of -10, and the years reigning 0", () => {
     test("Then it should throw the error 'Error: the amount of years must be a number bigger than 0'", () => {
       const yearsReigning = 0;
-      const age = 1;
+      const age = -10;
       const surname = "Rodríguez";
       const name = "Marcos";
 
       expect(() =>
         createKingCharacter(name, surname, age, yearsReigning),
-      ).toThrow();
+      ).toThrow("Error: the amount of years must be a number bigger than 0");
     });
   });
 });
