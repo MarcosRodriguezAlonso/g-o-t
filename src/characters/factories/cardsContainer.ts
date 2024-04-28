2e98fcff3875e972b3b2af0eaf361d527f8e759d
Marcos Rodríguez <e.marcosra@gmail.com>
2024-04-28 16:54:14 +0200

    Append cards container to root

--- src/characters/factories/cardsContainer.ts
+++ src/characters/factories/cardsContainer.ts
@@ -1,10 +1,13 @@
 export const getCardContainer = (): void => {
   const cards = document.querySelectorAll(".character-card");
   const cardsContainer = document.createElement("div");
+  const mainDiv = document.querySelector(".root")!;
+
+  cardsContainer.classList.add("card-container");
 
   cards.forEach((card) => {
     cardsContainer.appendChild(card);
   });
 
-  document.body.appendChild(cardsContainer);
+  mainDiv.appendChild(cardsContainer);
 };
